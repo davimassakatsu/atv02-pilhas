4d0ae95b9bb0f700e296dc3763240575f12c0b8b
Davi Massakatsu <davi.massakatsu@edu.unifil.br>
2026-03-23 22:48:19 -0300

    Commit MAIN

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------ PILHA SIMPLES ------");
+        System.out.println("============ PILHA SIMPLES ==========");
         PilhaSimples pilhaSimples = new PilhaSimples(5);
 
-        pilhaSimples.empilhar("10");
         pilhaSimples.empilhar("20");
+        pilhaSimples.empilhar("10");
         pilhaSimples.empilhar("30");
         pilhaSimples.exibir();
         pilhaSimples.mostrarTopo();
@@ -13,21 +13,21 @@ public class Main {
         pilhaSimples.desempilhar();
         pilhaSimples.desempilhar();
         pilhaSimples.desempilhar();
-        pilhaSimples.empilhar("40");
+        pilhaSimples.empilhar("10");
+        pilhaSimples.empilhar("30");
         pilhaSimples.empilhar("50");
-        pilhaSimples.empilhar("60");
         pilhaSimples.empilhar("70");
-        pilhaSimples.empilhar("80");
         pilhaSimples.empilhar("90");
+        pilhaSimples.empilhar("110");
         pilhaSimples.exibir();
         pilhaSimples.mostrarTopo();
 
-        System.out.println("\n------ PILHA DINÂMICA ------");
+        System.out.println("\n============ PILHA DINÂMICA ============");
         PilhaDinamica pilhaDinamica = new PilhaDinamica();
 
-        pilhaDinamica.empilhar("Arroz");
-        pilhaDinamica.empilhar("Feijão");
-        pilhaDinamica.empilhar("Batata");
+        pilhaDinamica.empilhar("Banana");
+        pilhaDinamica.empilhar("Maçã");
+        pilhaDinamica.empilhar("Pera");
         pilhaDinamica.exibir();
         pilhaDinamica.mostrarTopo();
         pilhaDinamica.quantidadeElementos();
@@ -35,8 +35,8 @@ public class Main {
         pilhaDinamica.desempilhar();
         pilhaDinamica.desempilhar();
         pilhaDinamica.desempilhar();
-        pilhaDinamica.empilhar("Macarrão");
-        pilhaDinamica.empilhar("Carne");
+        pilhaDinamica.empilhar("Kiwi");
+        pilhaDinamica.empilhar("Pitaya");
         pilhaDinamica.exibir();
         pilhaDinamica.mostrarTopo();
     }
